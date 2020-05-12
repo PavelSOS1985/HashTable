ef1dfa1c815ff2461e2fd6c25a9ccf31aced560d
PavelSOS <sawel@yandex.ru>
2020-05-12 13:42:02 +0800

    second cmt

--- src/HashTable.java
+++ src/HashTable.java
@@ -12,27 +12,28 @@ public class HashTable {
 
     // всегда возвращает корректный индекс слота
     public int hashFun(String value) {
-        byte[] arrBytesValue = value.getBytes();
+        byte[] arrBytesValue = value.getBytes();    // записываем байты элемента в массив
         int sum = 0;
         for (byte i :
                 arrBytesValue) {
-            sum += i;
+            sum += i;                               // считаем сумму байт
         }
         return sum % size;
     }
 
     // находит индекс пустого слота для значения, или -1
     public int seekSlot(String value) {
-        int indexSlot = this.hashFun(value);
-        int countPasses = 0;
+        int indexSlot = this.hashFun(value);                        // находим индекс элемента
+        int countPasses = 0;                                        // вспомогательная переменная отвечающая за количество проходов по слотам
         while (slots[indexSlot] != null) {
-            if (slots[indexSlot].equals(value)) return indexSlot;
-            indexSlot += step;
-            if (indexSlot >= size && countPasses < step) {
-                indexSlot = countPasses;
+            if (slots[indexSlot].equals(value))
+                return indexSlot;                                   // если слот содержит такой же элемент то возвращать индекс этого слота
+            indexSlot += step;                                      // изменение индекса слота на шаг
+            if (indexSlot >= size && countPasses < step) {          // если индекс выходит за пределы таблицы и проходов меньше чем необходимо
+                indexSlot = countPasses;                            // начинаем очередной проход сначала таблицы
                 countPasses++;
             }
-            if (indexSlot >= size && countPasses >= step) {
+            if (indexSlot >= size && countPasses >= step) {         // если вышли за пределы и прошли все слоты возвращаем -1
                 return -1;
             }
         }
@@ -49,6 +50,7 @@ public class HashTable {
 
     // находит индекс слота со значением, или -1
     public int find(String value) {
-        return this.seekSlot(value);
+        int i = this.seekSlot(value);
+        return i == -1 ? -1 : slots[i] == null ? -1 : i;
     }
 }
